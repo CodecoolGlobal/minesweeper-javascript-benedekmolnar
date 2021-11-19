cd4e3c5713fe980f4330f51410a6e883edd86118
benedekmolnar <molnar.benedek@gmail.com>
2021-11-19 10:21:27 +0100

    Write numbers into attributes

--- js/game.js
+++ js/game.js
@@ -15,18 +15,20 @@ const game = {
         const cols = parseInt(urlParams.get('cols'));
         const mineCount = parseInt(urlParams.get('mines'));
         const minePlaces = this.getRandomMineIndexes(mineCount, cols, rows);
+        let allTheShit = []
 
         let gameField = document.querySelector(".game-field");
         this.setGameFieldSize(gameField, rows, cols);
         let cellIndex = 0
         for (let row = 0; row < rows; row++) {
             const rowElement = this.addRow(gameField);
-            console.log(rowElement)
             for (let col = 0; col < cols; col++) {
                 this.addCell(rowElement, row, col, minePlaces.has(cellIndex));
                 cellIndex++;
+                allTheShit.push(rowElement.lastChild)
             }
         }
+        this.numberCell(allTheShit, cols)
     },
     getRandomMineIndexes: function (mineCount, cols, rows) {
         const cellCount = cols * rows;
@@ -52,15 +54,36 @@ const game = {
             'beforeend',
             `<div class="field${isMine ? ' mine' : ''}"
                         data-row="${row}"
-                        data-col="${col}"></div>`);
+                        data-col="${col}"
+                        ></div>`);
     },
+    numberCell: function (rowElement, width) {
+            for (let i = 0; i < rowElement.length; i++) {
+                let total = 0;
+                const isLeftEdge = (i % width === 0)
+                const isRightEdge = (i % width === width -1)
+                if (!rowElement[i].classList.contains("mine")) {
+                    if (i > 0 && !isLeftEdge && rowElement[i -1].classList.contains("mine")) total ++;
+                    if (i > 9 && !isRightEdge && rowElement[i +1 -width].classList.contains("mine")) total ++;
+                    if (i > 10 && rowElement[i -width].classList.contains("mine")) total ++;
+                    if (i > 11 && !isLeftEdge && rowElement[i -1 -width].classList.contains("mine")) total ++;
+                    if (i < 98 && !isRightEdge && rowElement[i +1].classList.contains("mine")) total ++;
+                    if (i < 90 && !isLeftEdge && rowElement[i -1 +width].classList.contains("mine")) total ++;
+                    if (i < 88 && !isRightEdge && rowElement[i +1 +width].classList.contains("mine")) total ++;
+                    if (i < 89 && rowElement[i +width].classList.contains("mine")) total ++;
+                    rowElement[i].setAttribute("data-number", total)
+                }
+        }
+    },
+
     initRightClick() {
         const fields = document.querySelectorAll('.game-field .row .field');
         fields.forEach( field => {
             field.addEventListener("contextmenu", event => {
-                if (!event.currentTarget.classList.contains("opened") && !event.currentTarget.classList.contains("boom"))
-                event.preventDefault();
-                event.currentTarget.classList.toggle("flagged");
+                if (!event.currentTarget.classList.contains("opened") && !event.currentTarget.classList.contains("boom")) {
+                    event.preventDefault();
+                    event.currentTarget.classList.toggle("flagged");
+                }
             });
         })
     },
@@ -72,7 +95,7 @@ const game = {
                 if (!event.currentTarget.classList.contains("flagged")) {
                     event.currentTarget.classList.add("opened");
                 }
-                if (event.currentTarget.classList.contains("mine")) {
+                if (event.currentTarget.classList.contains("mine") && !event.currentTarget.classList.contains("flagged")) {
                     event.currentTarget.classList.add("boom");
                 }
             });
